b33895cea8107af2f268a11d12f75fa38fb60b7c
dorothea <dorothea.reher@cocoonr.fr>
2023-12-20 11:37:31 +0100

    create directory battery and create Battery and the battery types

---
+++ batteries/battery.py
@@ -0,0 +1,6 @@
+class Battery:
+    def __init__(self, battery_type):
+        self.battery_type = battery_type
+
+    def needs_service(self):
+        pass
---
+++ batteries/spindler_battery.py
@@ -0,0 +1,13 @@
+from battery import Battery
+
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date, current_date):
+        super().__init__(last_service_date)
+        self.current_date = current_date
+
+    def needs_service(self):
+        pass
+
+    def battery_should_be_serviced(self):
+        return self.last_service_date.replace(year=self.last_service_date.year + 2)
